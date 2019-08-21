546ff12a53533e282ce9944f5039db63a195c01f
Nuzze <mati280341@gmail.com>
2019-08-21 12:03:18 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,13 +4,6 @@ const app = new Vue ({
   data: {
     name: 'F5Now?',
     version: '0.9.0',
-    background: '',
-    status: '',
-    description1: '',
-    description2: 'We are an open source project.',
-    description3: 'We are from Argentina but expanding us.',
-    credits1: 'Made with ',
-    credits2: 'by a geek team from Buenos Aires.',
     social: [
       {name: 'Twitter' , link: '' , icon: 'fab fa-twitter-square'},
       {name: 'Instagram' , link: '' , icon: 'fab fa-instagram'},
@@ -18,23 +11,95 @@ const app = new Vue ({
     ],
     complejos: [
       {
+        name: 'Predio 98',
+        id: 'predioNoventaOcho',
+        search: 'predio noventa y ocho',
+        rate: '4.4',
+        location: 'Calle 16 y 98',
+        canchas: '15',
+        futbol: 'F5 F7 F11',
+        cesped: 'Sintético y Natural',
+        infraestructura: 'Abierto',
+        background: 'http://www.predio98.com.ar/imagenes/logo2.png',
+        reserva: 'https://api.whatsapp.com/send?phone=5492216258871'
+      },
+      {
+        name: 'Línea de tres',
+        id: 'lineaDeTres',
+        search: 'linea de tres linea de 3',
+        rate: '4.0',
+        location: 'Calle 11 n° e/ 70 y 71',
+        canchas: '2',
+        futbol: 'F5',
+        cesped: 'Sintético',
+        infraestructura: 'Abierto',
+        background: 'https://scontent.faep5-1.fna.fbcdn.net/v/t1.0-9/30227103_453936681703789_3308582642527043271_n.jpg?_nc_cat=107&_nc_oc=AQmtBrYhQBQ_mT75KALW7Uh2PhV2MVAP3oggUvW7p7YZXUmkdgSATmZxzHUIKCMR0ZKckovl6e3lBp7iK_g5luLu&_nc_ht=scontent.faep5-1.fna&oh=cb494541be7adbf7ef980c26a20b99cc&oe=5DDFCA9D',
+        reserva: 'https://api.whatsapp.com/send?phone=5492216737686'
+      },
+      {
+        name: 'La Caldera del Diablo',
+        id: 'laCalderaDelDiablo',
+        search: 'caldera diablo',
+        rate: '4.3',
+        location: 'Calle 77 n°1130 e/ 17 y 18',
+        canchas: '3',
+        futbol: 'F5',
+        cesped: 'Sintético',
+        infraestructura: 'Techado',
+        background: 'http://www.laguiax.com.ar/media/W1siZiIsInByb2R1Y3Rpb24vZ3VpYXMvMS9jb21lcmNpb3MvOTgxOS9mb3Rvcy9hcmNoaXZvLTg3MjEuanBlZyJdLFsicCIsInRodW1iIiwiOTAweDQ1MFx1MDAzZSJdXQ/IMG_8117.JPG?sha=bb3f8bc4713d5aa6',
+        reserva: '0221 453-0955'
+      },
+      {
+        name: 'El Ayuntamiento',
+        id: 'elAyuntamiento',
+        search: '',
+        rate: '3.9',
+        location: 'Av. 1 n°777 e/ 47 y 48',
+        canchas: '2',
+        futbol: 'F5',
+        cesped: 'Sintético',
+        infraestructura: 'Techado',
+        background: '',
+        reserva: '0221 425-4994'
+      },
+      {
+        name: 'La Posta',
+        id: 'laPosta',
+        search: '',
+        rate: '4.0',
+        location: 'Av. 1 n°1375 e/ 60 y 61',
+        canchas: '1',
+        futbol: 'F5',
+        cesped: 'Sintético',
+        infraestructura: 'Techado',
+        background: 'https://10619-2.s.cdn12.com/rests/original/102_323154611.jpg',
+        reserva: '0221 482-3499'
+      },
+      {
         name: 'Camp Nou',
-        rate: '4.2 (Google)',
-        location: '13 e 68 y 69',
-        canchas: '4 canchas',
-        jugadores: '5 jugadores',
-        servicios: 'Vestuario',
+        id: 'campNou',
+        search: 'camp now castañeda',
+        rate: '4.2',
+        location: 'Calle 13 n° 863 e 68 y 69',
+        canchas: '4',
+        futbol: 'F5',
+        cesped: 'Sintético',
+        infraestructura: 'Abierto',
         background: 'https://lh3.googleusercontent.com/b5QGuQpOScBt33T2C8C2rFTefJwFLfG7cPsaMq_ihD8XzGLgjUVbLsT41KnOGfulgPpcn09u=w600-h0',
-        whatsapp: 'https://api.whatsapp.com/send?phone=5492213606778'
+        reserva: 'https://api.whatsapp.com/send?phone=5492213606778'
       },
       {
         name: 'Abran cancha',
-        rate: '4.8 (Google)',
-        location: '7 e 72 y 73',
-        canchas: '2 canchas',
-        jugadores: '5 jugadores',
-        servicios: 'Vestuario Bar/Restaurante Parrilla',
-        background: 'https://scontent.faep9-2.fna.fbcdn.net/v/t1.0-9/54268275_1012105125655220_2025072574643306496_n.jpg?_nc_cat=101&_nc_oc=AQlXBBjeFa5o3bIVfRMnMORtojxtCuycua236zkWEkCXNq-jAebEzFpYOXduKAXw7pQ&_nc_ht=scontent.faep9-2.fna&oh=4111e159c42ef2906f1ec177ab39595f&oe=5DDE2615'
+        id: 'abranCancha',
+        search: '',
+        rate: '4.8',
+        location: 'Calle 7 n° 1972 e 72 y 73',
+        canchas: '1 cancha',
+        futbol: 'F5',
+        cesped: 'Sintético',
+        infraestructura: 'Techado',
+        background: 'https://scontent.faep9-2.fna.fbcdn.net/v/t1.0-9/54268275_1012105125655220_2025072574643306496_n.jpg?_nc_cat=101&_nc_oc=AQlXBBjeFa5o3bIVfRMnMORtojxtCuycua236zkWEkCXNq-jAebEzFpYOXduKAXw7pQ&_nc_ht=scontent.faep9-2.fna&oh=4111e159c42ef2906f1ec177ab39595f&oe=5DDE2615',
+        reserva: ''
       }
     ]
   }
@@ -73,8 +138,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btnContainer = document.getElementById("btn-container");
+var btns = document.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
@@ -98,4 +163,22 @@ function search() {
             li[i].style.display = "none";
         }
     }
+}
+
+function menu() {
+  document.getElementById("menuOpen").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
